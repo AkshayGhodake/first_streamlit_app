6bbe40e61fbd39df0c8246fd7c7112896476cd6c
AkshayGhodake <125664377+AkshayGhodake@users.noreply.github.com>
2023-02-17 18:34:56 +0530

    Update streamlit_app.py
    Fixed Spelling mistake

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy Dinner')
 streamlit.header('Breakfast Menu')
-sgtreamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('Omega 3 and blueberry oatmeal')
 streamlit.text('kale Spinach & Rocket smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
