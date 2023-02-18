53e903337c24ba4e4974ab2f0db80202e6e31379
AkshayGhodake <125664377+AkshayGhodake@users.noreply.github.com>
2023-02-18 21:32:56 +0530

    Update streamlit_app.py
    Adding Request lib

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+
 streamlit.title('My Parents new healthy Dinner')
 
 streamlit.header('Breakfast Menu')
@@ -12,3 +13,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
+
+import requests
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
